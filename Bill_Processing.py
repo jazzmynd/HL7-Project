0e2f9f96d1c50c4d688e9201c87aa400070785ba
jazzmynd <43584405+jazzmynd@users.noreply.github.com>
2023-09-22 17:55:52 -0500

    pandas bill count

--- Bill_Processing.py
+++ Bill_Processing.py
@@ -1,49 +1,29 @@
-import csv
-from collections import defaultdict
-import os
+import pandas as pd
 
-# input CSV file
+# Load the CSV file into a DataFrame
 file_path = "/Users/jasmined/Desktop/HL7-Project/Archive/Original/sampledata.csv"
+df = pd.read_csv(file_path)
 
-# Initialize a defaultdict to store the total bill amount for each state
-total_bill_per_state = defaultdict(float)
+# Replace NaN, empty strings, or whitespaces in 'patient_state' with 'Unknown'
+df.loc[df['patient_state'].str.strip() == '', 'patient_state'] = 'Unknown'
 
-# Initialize a variable to store the overall total bill amount
-overall_total_bill = 0.0
 
-# Open the CSV file and create a DictReader object
-with open(file_path, mode='r') as file:
-    csv_reader = csv.DictReader(file)
-    
-    # Process each row in the CSV data
-    for row in csv_reader:
-        # Get the state and bill amount from the row
-        state = row['patient_state'].strip()  # strip() is used to remove leading and trailing whitespaces
-        bill_amount = float(row['bill_amount'])
-        
-        # Handle missing state 
-        if not state:
-            state = "Unknown"
-        
-        # Add the bill amount to the total for the state
-        total_bill_per_state[state] += bill_amount
-        
-        # Add the bill amount to the overall total bill amount
-        overall_total_bill += bill_amount
+# Group by the 'patient_state' column and calculate the sum of 'bill_amount' for each group
+total_bill_per_state = df.groupby('patient_state')['bill_amount'].sum()
+
+# Calculate the overall total bill amount
+overall_total_bill = total_bill_per_state.sum()
 
 # Specify the report file path
-report_file_path = "/Users/jasmined/Desktop/HL7-Project/Archive/Modified/Sampledata_Bill_Amount.txt"
+report_file_path = "/Users/jasmined/Desktop/HL7-Project/Archive/Modified/sample_data_report.txt"
 
-# Create the directory if it does not exist
-os.makedirs(os.path.dirname(report_file_path), exist_ok=True)
-
-# Create and write to the report file
+# Open the report file in write mode and write the results
 with open(report_file_path, 'w') as report_file:
-    # Write the total bill amount for each state to the report file
+    # Write the total bill amount for each state
     for state, total_bill in total_bill_per_state.items():
         report_file.write(f'{state}: {total_bill}\n')
     
-    # Write the overall total bill amount to the report file
+    # Write the overall total bill amount
     report_file.write(f'Total Bill Amount: {overall_total_bill}\n')
 
 print(f'Report file has been created successfully at {report_file_path}.')
