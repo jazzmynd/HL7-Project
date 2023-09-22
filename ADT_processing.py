740db274b287b4b67644eaf3e8227879d72a7533
jazzmynd <43584405+jazzmynd@users.noreply.github.com>
2023-09-22 10:58:05 -0500

    add SQLite

--- ADT_processing.py
+++ ADT_processing.py
@@ -3,7 +3,140 @@ import string
 import os
 from datetime import date
 from collections import defaultdict
+import sqlite3
 from output_files_creation import create_output_files, modified_dir, adt_modified_file
+
+# --- SQLite Setup ---
+
+def setup_database():
+    conn = sqlite3.connect('adt_data.db')
+    cursor = conn.cursor()
+
+    # Create a table for ADT data
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS adt_records (
+        id INTEGER PRIMARY KEY,
+        patient_id TEXT,
+        site_id TEXT,
+        service_location TEXT,
+        date_of_service TEXT,
+        message_type TEXT,
+        message_time TEXT,
+        message_id TEXT,
+        account_number TEXT,
+        discharge_disposition TEXT,
+        financial_class TEXT,
+        patient_first_name TEXT,
+        patient_last_name TEXT,
+        patient_middle_name TEXT,
+        patient_name TEXT,
+        patient_address_1 TEXT,
+        patient_address_2 TEXT,
+        patient_city TEXT,
+        patient_state TEXT,
+        patient_zip TEXT,
+        patient_zip4 TEXT,
+        patient_date_of_birth TEXT,
+        patient_deceased_date TEXT,
+        patient_sex TEXT,
+        patient_ssn TEXT,
+        referring_doctor_id TEXT,
+        attending_doctor_id TEXT,
+        patient_ethnicity TEXT,
+        patient_race TEXT,
+        patient_language TEXT,
+        patient_smoking_status TEXT,
+        patient_email_address TEXT,
+        patient_cell_phone_area_code TEXT,
+        patient_cell_phone_number TEXT,
+        patient_marital_status TEXT,
+        bill_amount REAL,
+        patient_drivers_license_number TEXT,
+        guarantor_first_name TEXT,
+        guarantor_last_name TEXT,
+        guarantor_middle_name TEXT,
+        guarantor_address_1 TEXT,
+        guarantor_address_2 TEXT,
+        guarantor_city TEXT,
+        guarantor_state TEXT,
+        guarantor_zip TEXT
+    )
+    ''')
+
+    conn.commit()
+    conn.close()
+
+setup_database()
+
+def insert_to_database(data):
+    conn = sqlite3.connect('adt_data.db')
+    cursor = conn.cursor()
+
+    # Remove the '#' key from the data dictionary if it exists
+    if '#' in data:
+        del data['#']
+
+ # List of all column names except the 'id' since it is auto-incremented
+    all_columns = [
+    'patient_id', 'site_id', 'service_location', 'date_of_service', 'message_type',
+    'message_time', 'message_id', 'account_number', 'discharge_disposition',
+    'financial_class', 'patient_first_name', 'patient_last_name', 'patient_middle_name',
+    'patient_name', 'patient_address_1', 'patient_address_2', 'patient_city', 
+    'patient_state', 'patient_zip', 'patient_zip4', 'patient_date_of_birth', 
+    'patient_deceased_date', 'patient_sex', 'patient_ssn', 'referring_doctor_id', 
+    'attending_doctor_id', 'patient_ethnicity', 'patient_race', 'patient_language', 
+    'patient_smoking_status', 'patient_email_address', 'patient_cell_phone_area_code', 
+    'patient_cell_phone_number', 'patient_marital_status', 'bill_amount', 
+    'patient_drivers_license_number', 'guarantor_first_name', 'guarantor_last_name', 
+    'guarantor_middle_name', 'guarantor_address_1', 'guarantor_address_2', 'guarantor_city', 
+    'guarantor_state', 'guarantor_zip'
+    ]
+
+     # Check if any keys in data are not in all_columns
+    extra_keys = set(data.keys()) - set(all_columns)
+    if extra_keys:
+        raise ValueError(f"data contains keys not in all_columns: {extra_keys}")
+    
+
+    # Check each column, if it is not in data, add it with the value "n/a"
+    for column in all_columns:
+        if column not in data:
+            data[column] = "n/a"
+    # Insert ADT data into the table
+    cursor.execute('''
+    INSERT INTO adt_records (
+        patient_id, site_id, service_location, date_of_service, message_type, 
+        message_time, message_id, account_number, discharge_disposition, 
+        financial_class, patient_first_name, patient_last_name, patient_middle_name, 
+        patient_name, patient_address_1, patient_address_2, patient_city, 
+        patient_state, patient_zip, patient_zip4, patient_date_of_birth, 
+        patient_deceased_date, patient_sex, patient_ssn, referring_doctor_id, 
+        attending_doctor_id, patient_ethnicity, patient_race, patient_language, 
+        patient_smoking_status, patient_email_address, patient_cell_phone_area_code, 
+        patient_cell_phone_number, patient_marital_status, bill_amount, 
+        patient_drivers_license_number, guarantor_first_name, guarantor_last_name, 
+        guarantor_middle_name, guarantor_address_1, guarantor_address_2, guarantor_city, 
+        guarantor_state, guarantor_zip
+    )
+    VALUES (
+        :patient_id, :site_id, :service_location, :date_of_service, :message_type, 
+        :message_time, :message_id, :account_number, :discharge_disposition, 
+        :financial_class, :patient_first_name, :patient_last_name, :patient_middle_name, 
+        :patient_name, :patient_address_1, :patient_address_2, :patient_city, 
+        :patient_state, :patient_zip, :patient_zip4, :patient_date_of_birth, 
+        :patient_deceased_date, :patient_sex, :patient_ssn, :referring_doctor_id, 
+        :attending_doctor_id, :patient_ethnicity, :patient_race, :patient_language, 
+        :patient_smoking_status, :patient_email_address, :patient_cell_phone_area_code, 
+        :patient_cell_phone_number, :patient_marital_status, :bill_amount, 
+        :patient_drivers_license_number, :guarantor_first_name, :guarantor_last_name, 
+        :guarantor_middle_name, :guarantor_address_1, :guarantor_address_2, :guarantor_city, 
+        :guarantor_state, :guarantor_zip
+    )
+    ''', data)
+    conn.commit()
+    conn.close()
+
+# ---------------------
 
 create_output_files('ADT')
 
@@ -31,7 +164,7 @@ guarantor_zip = ""
 csv_columns = [
     "#", "patient_id", "site_id", "service_location","date_of_service", "message_type", "message_time", "message_id",
     "account_number", "discharge_disposition", "financial_class", "patient_first_name",
-    "patient_last_name", "patient_middle_name","*patient_name","patient_address_1", "patient_address_2",
+    "patient_last_name", "patient_middle_name","patient_name","patient_address_1", "patient_address_2",
     "patient_city", "patient_state", "patient_zip", "patient_zip4", "patient_date_of_birth",
     "patient_deceased_date", "patient_sex", "patient_ssn", "referring_doctor_id",
     "attending_doctor_id", "patient_ethnicity", "patient_race", "patient_language",
@@ -97,7 +230,7 @@ for segment in segments:
         csv_data["patient_first_name"] = name_fields[1]
         csv_data["patient_last_name"] = name_fields[0]
         csv_data["patient_middle_name"] = name_fields[2]
-        csv_data['*patient_name'] = name_fields[0] +' '+ name_fields[1] +' '+ name_fields[2]
+        csv_data['patient_name'] = name_fields[0] +' '+ name_fields[1] +' '+ name_fields[2]
 
         # patient address1
         address_fields = fields[11].split('^')
@@ -204,6 +337,8 @@ csv_data["guarantor_city"] = guarantor_city
 csv_data["guarantor_state"] = guarantor_state
 csv_data["guarantor_zip"] = guarantor_zip
 
+# After writing the extracted data to the output CSV file
+insert_to_database(csv_data)
 
 # Path to the output CSV file
 output_csv_file_path = os.path.join(modified_dir, adt_modified_file)
--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 # 1. The objective of this project is to match information from an HL7 ADT text file to a CSV file and, upon finding a match, add additional columns to the text file using data from the CSV.
 
 
+# Execute the DELETE command to remove duplicate rows
+cursor.execute('DELETE FROM adt_records WHERE rowid NOT IN (SELECT MIN(rowid) FROM adt_records GROUP BY patient_id);')
